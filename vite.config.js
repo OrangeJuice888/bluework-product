950f87dd6f788938ddaee33d3223b0deffb01c13
Owen Jordan <owen.jordans@icloud.com>
2026-07-06 20:48:56 +1000

    Testing push

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/bluework-product/',
-  // server: {
-  //   port: 5273,
-  //   strictPort: true,
-  // },
 })
