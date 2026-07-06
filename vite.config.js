630d25d92cfa81ac9ca8adf3380aaf800e6e4701
Owen Jordan <owen.jordans@icloud.com>
2026-07-06 11:50:23 +1000

    Blank page issue fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bluework-product/',
+  base: '/bluework-product',
   // server: {
   //   port: 5273,
   //   strictPort: true,
